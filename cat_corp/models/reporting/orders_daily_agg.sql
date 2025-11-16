d12df3d5b0db788d787270f5b1d0f8fd737fbdab
mrintern <nicho.santo3@gmail.com>
2025-11-17 00:07:51 +0100

    checkpoint

--- cat_corp/models/reporting/orders_daily_agg.sql
+++ cat_corp/models/reporting/orders_daily_agg.sql
@@ -1,6 +1,7 @@
 
 {{ config(materialized='table') }}
 
-select date,listing_id, round(avg(orders),2) as avg_orders
+select date,listing_id, sum(orders) as total_orders
+
     from {{ref('orders_daily_cleaned')}}
 GROUP BY date,listing_id
--- cat_corp/models/staging/orders_daily_cleaned.sql
+++ cat_corp/models/staging/orders_daily_cleaned.sql
@@ -2,3 +2,4 @@
 {{ config(materialized='table') }}
 
 select DISTINCT * from {{ ref('orders_daily') }}
+where orders >= 0
